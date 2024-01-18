20e472eff2e08bc68b75a05bc25572b1ecffc669
maxcurrent420 <125443558+maxcurrent420@users.noreply.github.com>
2024-01-18 10:50:10 -0600

    Update inventory.js

--- components/inventory.js
+++ components/inventory.js
@@ -1,25 +1,48 @@
-import { Item } from './item.js';
-
+// Inventory class
 class Inventory {
+  maxItems = 500;
+
   constructor() {
     this.items = [];
-    this.maxItems = 500;
+    maxItems = 500;
   }
 
+  // Add item to inventory
   addItem(item) {
     if (this.items.length < this.maxItems) {
       this.items.push(item);
     } else {
-      throw new Error('Inventory is full');
+      console.log('Inventory is full');
     }
+    this.items.push(item);
   }
 
+  // Use item from inventory
   useItem(item) {
     const index = this.items.indexOf(item);
-    if (index > -1) {
+    if(index > -1) {
+      this.items.splice(index, 1);
+    }
+    const index = this.items.indexOf(item);
+    if(index > -1) {
       this.items.splice(index, 1);
     }
   }
+
 }
 
-export default Inventory;
+// Item class
+class Item {
+
+  constructor(name, type) {
+    this.name = name;
+    this.type = type; 
+  }
+
+}
+
+// Export classes
+export {
+  Inventory,
+  Item
+}
